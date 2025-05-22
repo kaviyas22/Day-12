6c849c0135bcc59ca8850d92585522a22e978911
kaviyas22 <shreekaviya862@gmail.com>
2025-05-22 21:46:16 +0530

    Add files via upload

---
+++ Transportation.java
@@ -0,0 +1,18 @@
+
+package transportation;
+
+
+public class Transportation {
+    public void bus(){
+        char 'b';
+        b=b+'u'+'s'; 
+        System.out.println("transportation vehicle:"+b);
+    }
+    
+    public static void main(String[] args) {
+        Transportation t=new Transportation();
+        t.bus();
+        // TODO code application logic here
+    }
+    
+}
