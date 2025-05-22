16eb7f76c7b35695a6bdc5deb167ec0be50ecac4
kaviyas22 <shreekaviya862@gmail.com>
2025-05-22 21:41:12 +0530

    Add files via upload

---
+++ Flower.java
@@ -0,0 +1,18 @@
+
+package flower;
+
+public class Flower {
+    public void  blooming(){
+        String a="rose";
+        a=a+" blooms";
+        System.out.println(a+" in summer");
+    
+}
+    public static void main(String[] args) {
+        Flower F= new Flower();
+        F.blooming();
+                
+        
+    }
+    
+}
